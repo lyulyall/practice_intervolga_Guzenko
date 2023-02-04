8e604d1718a3342882509ea1373f26b8a6045330
Julia Guzenko <prib-201_432493@volsu.ru>
2023-02-05 00:21:28 +0300

    Рейтинг по группам

--- php/logic.php
+++ php/logic.php
@@ -11,17 +11,6 @@ function getData($sql)
 	return $result;
 }
 
-
-function rating($stud_id, $subj_id)
-{
-	$sql = "SELECT grades.grade, subjects.subject_name as 'subject',  students.id
-    FROM `grades`
-    JOIN `subjects` ON grades.subject_id=subjects.id JOIN `students` ON grades.student_id=students.id
-    WHERE grades.student_id=$stud_id
- 	ORDER BY students.id, subjects.id ";
-
-	return getData($sql);
-}
 function grades()
 {
 	$sql = "SELECT grades.grade, subjects.subject_name as 'subject', students.surname, students.id,
@@ -43,6 +32,24 @@ function allSubjects()
 function allData($table)
 {
 	$sql = "SELECT * FROM $table";
+
+	return getData($sql);
+}
+
+function ratingGroupGrades($stud_id, $group)
+{
+	$sql = "SELECT grades.grade, subjects.subject_name as 'subject',  students.id, students.name, students.surname
+    FROM `grades`
+    JOIN `subjects` ON grades.subject_id=subjects.id JOIN `students` ON grades.student_id=students.id
+    WHERE students.group_id=$group AND grades.student_id=$stud_id 
+ 	ORDER BY students.id, subjects.id ";
+
+	return getData($sql);
+}
+
+function ratingGroupStudents($group)
+{
+	$sql = "SELECT * FROM `students` WHERE students.group_id=$group";
 
 	return getData($sql);
 }
--- php/rating.php
+++ php/rating.php
@@ -3,57 +3,50 @@ require_once 'header.php';
 require_once 'logic.php';
 require_once 'db.php';
 $groups = allData('groups');
-$students = allData('students');
 ?>
 
 <!--Рейтинг-->
 <div class="container main">
+    <form class="dropdown" method="get">
+        <label for="group">Выберите группу: </label>
+        <select required name="group"  class="form-select">
+            <?php foreach ($groups as $item): ?>
+                <option selected value="<?= $item['id'] ?>"> <?= $item['specialty']?> </option>
+            <?php endforeach; ?>
+        </select>
+        <button type="submit" class="btn btn-primary my-4" name="build">Построить рейтинг</button>
+    </form>
 
-    <!-- Выбор группы их выпадающего списка-->
-    <div class="dropdownbutton" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-expanded="false">
-        <div class="row justify-content-berween" role="button">
-            <div class="col-md-auto title">Выберите группу... </div>
-            <div class="col"></div>
-            <div class="col-md-auto"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                          class="bi bi-chevron-down" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1
-                    .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
-                </svg>
-            </div>
-        </div>
 
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-			<?php foreach ($groups as $item): ?>
-                <li><a id='<?= $item['id']?>' class="dropdown-item" href="#"> <?= $item['specialty']?></a></li>
-			<?php endforeach; ?>
-
-        </ul>
-    </div>
-
-	<?php if (count($students) > 0): ?>
-        <table class="table table-bordered">
-            <thead>
-            <tr>
-                <th class="scope">Cтуденты</th>
-				<?php $subjects=allSubjects(); ?>
-				<?php foreach ($subjects as $subj): ?>
-                    <th> <?= $subj['subject_name']?> </th>
-				<?php endforeach; ?>
-            </tr>
-            </thead>
-
-            <tbody>
-			<?php foreach ($students as $stud): ?>
+    <?php if (array_key_exists('build', $_GET)): ?>
+        <?php $students = ratingGroupStudents($_GET['group']);?>
+		<?php if (count($students) > 0): ?>
+            <table class="table table-bordered">
+                <thead>
                 <tr>
-                    <th> <?= $stud['surname']?> <?= $stud['name']?></th>
-					<?php $rating=rating($stud['id'],  $subj['id']); ?>
-					<?php foreach ($rating as $rat): ?>
-                        <td> <?=$rat['grade']?></td>
-					<?php endforeach; ?>
+                    <th class="scope">Cтуденты</th>
+				    <?php $subjects=allSubjects(); ?>
+				    <?php foreach ($subjects as $subj): ?>
+                        <th> <?= $subj['subject_name']?> </th>
+				    <?php endforeach; ?>
                 </tr>
-			<?php endforeach; ?>
-            </tbody>
-        </table>
+                </thead>
+
+                <tbody>
+				<?php foreach ($students as $stud): ?>
+                    <tr>
+						<?php $rating=ratingGroupGrades($stud['id'], $_GET['group']); ?>
+                        <th> <?= $stud['surname']?> <?= $stud['name']?></th>
+						<?php foreach ($rating as $rat): ?>
+                            <td> <?=$rat['grade']?></td>
+						<?php endforeach; ?>
+                    </tr>
+				<?php endforeach; ?>
+                </tbody>
+            </table>
+        <?php else: ?>
+            <?php echo 'Студентов нет' ?>
+	    <?php endif; ?>
 	<?php endif; ?>
 </div>
 
